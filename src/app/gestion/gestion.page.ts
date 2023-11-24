118acf5293988daacc68032bec448c6393836b99
Alexis <ale.floress@duocuc.cl>
2023-11-23 23:16:17 -0300

    Entrega3

--- src/app/gestion/gestion.page.ts
+++ src/app/gestion/gestion.page.ts
@@ -30,49 +30,4 @@ export class GestionPage implements OnInit {
     this.modal.dismiss(null, 'cancel');
   }
   
-  obtenerTodo() {
-    this.datosAPI = ""
-    this.api.getPosts().subscribe((res) => {
-      console.log(res);
-      res.forEach((tmp: dataAPI) => {
-        this.datosAPI += tmp.id + "\n";
-        this.datosAPI += tmp.nombre + "\n";
-      });
-    }, (error) => {
-      console.log(error);
-    })
-  }
-
-  eliminar(id: any) {
-    this.api.deletePost(id).subscribe((res) => {
-      this.datosAPI = "Eliminado con exito";
-    }, (err) => {
-      console.error(err);
-    })
-  }
-  modificar(id: any) {
-    this.api.getPost(id).subscribe((res: dataAPI) => {
-      console.log(res);
-      this.post.id = res.id;
-      this.post.nombre = res.nombre;
-
-
-    }, (err) => {
-      console.error(err.message)
-    })
-  }
-
-  cancelUpdateModal() {
-    this.updateModal.dismiss(null, 'cancel');
-  }
-
-  confirmUpdateModal() {
-    console.log(this.post);
-    this.api.updatePost(this.post.id, this.post).subscribe((success) => {
-      this.datosAPI = "Modificado con Exito  ";
-    }, (err) => {
-      console.error(err);
-    })
-    this.updateModal.dismiss(null, 'confirm');
-  }
 }
